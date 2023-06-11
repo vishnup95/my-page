00fcdfebd3d806c5bea8561f7f82337255e1dda1
Vishnu Prasad <50899727+vishnup95@users.noreply.github.com>
2023-06-11 17:11:49 +0530

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "@unocss",
+    "plugin:astro/jsx-a11y-recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/no-non-null-assertion": "off",
+      },
+    },
+    {
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# My Personal Astro Starter Kit
+
+This template will serve as my personal astro template.
